dc5ae3ce78ed55105961332330a76a73976a190c
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-25 14:39:16 +0700

    Feat: Added swagger configuration

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
-const mainRoutes = require('./src/routers/routes');
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+const authRoutes = require('./src/routers/auth.routes');
+const productRoutes = require('./src/routers/product.routes');
 
 const app = express();
+
+const swaggerOptions = {
+  info: {
+    version: '1.0.0',
+    title: 'API Documentation',
+    description: 'API Documentation untuk User Authentication dan Product Management',
+    contact: {
+      name: 'Developer',
+      email: 'dev@mail.com'
+    }
+  },
+  baseDir: __dirname,
+  filesPattern: './**/*.js',
+  swaggerUIPath: '/api-docs',
+  exposeSwaggerUI: true,
+  exposeApiDocs: false,
+  apiDocsPath: '/v3/api-docs',
+  notRequiredAsNullable: false
+};
+
+expressJSDocSwagger(app)(swaggerOptions);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/**
+ * @summary Home endpoint
+ * @return {object} 200
+ */
 app.get('/', (req, res) => {
   res.json({
     success: true,
@@ -13,11 +40,13 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use('/api', mainRoutes);
+app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
 
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`APP running on http://localhost:${PORT}`);
+  console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
 });
 
 module.exports = app;
