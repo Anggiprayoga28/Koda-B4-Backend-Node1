3306883b14897911940090ec963666d42fcf875f
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-26 23:08:09 +0700

    Refactor: Switch to Better-SQLite adapter and update PrismaClient initialization

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,11 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+import process from "process";
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
