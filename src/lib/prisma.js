2ceeff7102a958786e8fe5483bc1edf0b23d86fa
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-11-26 17:20:17 +0700

    Refactor: Convert to ES Modules and remove verbose Prisma logging

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,7 +1,6 @@
-const { PrismaClient } = require('@prisma/client');
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'],
-});
+const prisma = new PrismaClient();
 
-module.exports = prisma;
+export { prisma };
